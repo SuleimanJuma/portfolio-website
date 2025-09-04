c6adc26de67f67fe867497ddae5be45e03a8c43c
Suleiman <suleimanj225@gmail.com>
2025-09-04 12:46:05 +0300

    corrections

--- my portfolio/scripts/script.js
+++ my portfolio/scripts/script.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", () => {
   // Typing Animation for Hero Section
   const typingElement = document.querySelector(".typing-animation");
   if (typingElement) {
-  const text = "Building Tomorrow’s Digital World";
+  const text = "Building Tomorrow’s Digital World—Securely";
     const chars = Array.from(text);
     let i = 0;
     typingElement.textContent = "";
